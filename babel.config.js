dec42f879b5e8f19d91bb7ae92f8d475fe7469b9
Dolpho <arezchess@gmail.com>
2026-02-11 15:51:56 +0300

    Remove module-resolver plugin from Babel config
    Removed the module-resolver plugin configuration from Babel.

--- babel.config.js
+++ babel.config.js
@@ -2,16 +2,5 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
-    plugins: [
-      [
-        "module-resolver",
-        {
-          root: ["./"],          // project root
-          alias: {
-            "@": "./app"          // @/ → app folder
-          },
-        },
-      ],
-    ],
   };
 };
